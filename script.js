3db93e773d7f8a70a528c5afaa67220149db07b6
Sam Poder <sam.r.poder@gmail.com>
2020-09-13 08:45:54 +0000

    back we go

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 Tone.start();
 const synth = new Tone.Synth().toDestination();
-var section = 4
 document.onkeydown = function(e){
   e = e || window.event;
   var key = e.which || e.keyCode;
-  if(key===83){playNote('C', e.shiftKey)}
-  if(key===68){playNote('D', e.shiftKey)}
-  if(key===70){playNote('E', e.shiftKey)}
-  if(key===71){playNote('F', e.shiftKey)}
-  if(key===72){playNote('G', e.shiftKey)}
-  if(key===74){playNote('A', e.shiftKey)}
-  if(key===75){playNote('B', e.shiftKey)}
-  if(key===37){if(section>1){section-=1; document.getElementById('sectionNo').innerText = section}else{alert("The lowest keyboard section is 1.")}}
-  if(key===39){if(section<8){section+=1}else{alert("The highest keyboard section is 8.")}}
+  if(key===83){playNote('C')}
+  if(key===68){playNote('D')}
+  if(key===70){playNote('E')}
+  if(key===71){playNote('F')}
+  if(key===72){playNote('G')}
+  if(key===74){playNote('A')}
+  if(key===75){playNote('B')}
 }
-function playNote(note, shift) {
-  console.log(`${note}${(shift) ? '#' : ''}${section}`)
-  synth.triggerAttackRelease(`${note}${(shift) ? '#' : ''}4`, "8n");
-  document.getElementById(note).style.background = (shift) ? '#338eda' : '#33d6a6';
-  setTimeout(() => {  
-    document.getElementById(note).style.background = 'white'; 
-    }, 200);
+function playNote(note) {
+  synth.triggerAttackRelease(`${note}4`, "8n");
+  document.getElementById(note).style.background = '#33d6a6';
+  setTimeout(() => {  document.getElementById(note).style.background = 'white'; }, 200);
 }
